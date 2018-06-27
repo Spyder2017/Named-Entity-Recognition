a5b5d8e0eb51061428b3e4595e0f7e90c899fb6f
AlphaGod <977665949@qq.com>
2018-06-27 15:37:09 +0800

    Add files via upload
    将标注方式由BIO2转换为BIOES

---
+++ bio2bioes.py
@@ -0,0 +1,109 @@
+import codecs,re
+
+def iob2(tags):
+    """
+    Check that tags have a valid BIO format.
+    Tags in BIO1 format are converted to BIO2.
+    """
+    for i, tag in enumerate(tags):
+        if tag == 'O':
+            continue
+        split = tag.split('-')
+        if len(split) != 2 or split[0] not in ['I', 'B']:
+            return False
+        if split[0] == 'B':
+            continue
+        elif i == 0 or tags[i - 1] == 'O':  # conversion IOB1 to IOB2
+            tags[i] = 'B' + tag[1:]
+        elif tags[i - 1][1:] == tag[1:]:
+            continue
+        else:  # conversion IOB1 to IOB2
+            tags[i] = 'B' + tag[1:]
+    return True
+
+def iob_iobes(tags):
+    """
+    the function is used to convert
+    BIO -> BIOES tagging
+    """
+    new_tags = []
+    for i, tag in enumerate(tags):
+        if tag == 'O':
+            new_tags.append(tag)
+        elif tag.split('-')[0] == 'B':
+            if i + 1 != len(tags) and \
+               tags[i + 1].split('-')[0] == 'I':
+                new_tags.append(tag)
+            else:
+                new_tags.append(tag.replace('B-', 'S-'))
+        elif tag.split('-')[0] == 'I':
+            if i + 1 < len(tags) and \
+                    tags[i + 1].split('-')[0] == 'I':
+                new_tags.append(tag)
+            else:
+                new_tags.append(tag.replace('I-', 'E-'))
+        else:
+            raise Exception('Invalid IOB format!')
+    return new_tags
+
+def zero_digits(s):
+    """
+    Replace every digit in a string by a zero.
+    """
+    return re.sub('\d', '0', s)
+
+def update_tag_scheme(sentences, tag_scheme):
+    """
+    Check and update sentences tagging scheme to BIO2
+    Only BIO1 and BIO2 schemes are accepted for input data.
+    """
+    for i, s in enumerate(sentences):
+        tags = [w[-1] for w in s]
+        # Check that tags are given in the BIO format
+        if not iob2(tags):
+            s_str = '\n'.join(' '.join(w) for w in s)
+            raise Exception('Sentences should be given in BIO format! ' +
+                            'Please check sentence %i:\n%s' % (i, s_str))
+        if tag_scheme == 'BIOES':
+            new_tags = iob_iobes(tags)
+            for word, new_tag in zip(s, new_tags):
+                word[-1] = new_tag
+        else:
+            raise Exception('Wrong tagging scheme!')
+
+def load_sentences(path, zeros):
+    """
+    Load sentences. A line must contain at least a word and its tag.
+    Sentences are separated by empty lines.
+    """
+    sentences = []
+    sentence = []
+    for line in codecs.open(path, 'r', 'utf8'):
+        line = zero_digits(line.rstrip()) if zeros else line.rstrip()
+        if not line:
+            if len(sentence) > 0:
+                if 'DOCSTART' not in sentence[0][0]:
+                    sentences.append(sentence)
+                sentence = []
+        else:
+            word = line.split()
+            assert len(word) >= 2
+            sentence.append(word)
+    if len(sentence) > 0:
+        if 'DOCSTART' not in sentence[0][0]:
+            sentences.append(sentence)
+    return sentences
+
+if __name__ == '__main__':
+    path = 'E:\\HotEvent\\NER_corpus\\NER_corpus\\jnlpba2004\\ner\\train.txt'
+    bioes_path = 'E:\\HotEvent\\NER_corpus\\NER_corpus\\jnlpba2004\\ner\\bioes_train.txt'
+    sentences = load_sentences(path, True)
+    update_tag_scheme(sentences, 'BIOES')
+    sens_string = ''
+    for sentence in sentences:
+        for word_label in sentence:
+            sens_string = sens_string + word_label[0] + '\t' + word_label[1] + '\n'
+            print(word_label[0],word_label[1])
+        sens_string = sens_string + '\n'
+    with open(bioes_path,'wb') as f:
+        f.write(sens_string.encode('utf-8'))
